c4ac1495109cc3def343d757a4df6a2efa15fe60
Adrian Fernandez <adriianfdzm@gmail.com>
2025-11-29 17:48:10 +0100

    refactor: move CORS to env

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CORS_ORIGINS?.split(',') ?? ['http://localhost:3000', 'http://localhost:8000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
